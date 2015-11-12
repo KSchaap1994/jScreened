c1a78a123d13c50433499a70476bcd02ff3903ce
Kevin Schaap <kschaap1994@gmail.com>
2015-11-12 19:54:50 +0100

    ant changes.

--- src/org/jscreened/io/ScreenedImage.java
+++ src/org/jscreened/io/ScreenedImage.java
@@ -1,13 +1,14 @@
 package org.jscreened.io;
 
-import com.sun.org.apache.xml.internal.security.utils.Base64;
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.net.URLCodec;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.net.URLEncoder;
 
 /**
  * Created by Kevin on 11-11-2015.
@@ -26,9 +27,10 @@ public final class ScreenedImage {
             final ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
             ImageIO.write(image, "png", byteArray);
             final byte[] imageByte = byteArray.toByteArray();
-            final String dataImage = Base64.encode(imageByte);
-            data = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(dataImage, "UTF-8");
-        } catch (IOException e) {
+            final String dataImage = Base64.encodeBase64String(imageByte);
+            final URLCodec codec = new URLCodec("UTF-8");
+            data = String.format("%s=%s", codec.encode("image"), codec.encode(dataImage));
+        } catch (IOException | EncoderException e) {
             e.printStackTrace();
         }
     }
